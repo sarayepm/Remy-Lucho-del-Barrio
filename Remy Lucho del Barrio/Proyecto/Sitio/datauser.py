ec4e255d452ce070d8c8fd3448b0c328dd4d52f8
Saray Esperanza Peñaylillo Montecinos <s.penaylillomontecinos@liceorbl.cl>
2025-11-11 11:28:49 -0300

    Delete Remy Lucho del Barrio directory

--- Remy Lucho del Barrio/Proyecto/Sitio/datauser.py
+++
@@ -1,9 +0,0 @@
-def user():
-    usuario = {
-        'Nombre': "",
-        'Rango': "", #Estudiante, profesor, etc
-        'Edad': "",
-        'Rut': "",
-        'Estado de cuenta': "",
-        'Privada': False
-    }
